761cb9c89ea87dcabaa1d23c0cabe775073dd8e2
JD061905 <jd.echeverry@uniandes.edu.co>
2025-05-09 07:39:32 -0500

    ¿?

---
+++ DataStructures/Graph/digraph.py
@@ -0,0 +1,49 @@
+from DataStructures.Graph import edge
+from DataStructures.Graph import vertex
+from DataStructures.Map import map_linear_probing as mp
+
+def new_graph(order):
+    graph = {'vertices':'A'}
+    return 
+
+def insert_vertex():
+    return
+
+def update_vertex_info():
+    return
+
+def remove_vertex():
+    return
+
+def add_edge():
+    return
+
+def order():
+    return
+
+def size():
+    return
+
+def vertices():
+    return
+
+def degree():
+    return
+
+def get_edge():
+    return
+
+def get_vertex_information():
+    return
+
+def contains_vertex():
+    return
+
+def adjacents():
+    return
+
+def edges_vertex():
+    return
+
+def get_vertex():
+    return
