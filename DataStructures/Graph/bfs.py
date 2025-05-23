ad9a4d933b190f784ad346e91d56dbf17884aa3e
JD061905 <jd.echeverry@uniandes.edu.co>
2025-05-22 22:04:04 -0500

    bfs

--- DataStructures/Graph/bfs.py
+++ DataStructures/Graph/bfs.py
@@ -1,12 +1,16 @@
+from DataStructures.List import array_list as al
+from DataStructures.Queue import queue as q
+from DataStructures.Graph import digraph as G
+
 def bfs(grafo,source):
-    visited = []
-    queue = [source]
-    while len(queue) > 0:
-        elem = queue.pop(0)
-        if elem not in visited:
-            visited.append(elem)
-            for nodo in grafo[elem]:
-                if nodo not in visited:
-                    queue.append(nodo)
-                
+    visited = al.new_list()
+    queue = q.new_queue()
+    q.enqueue(queue,source)
+    while q.size(queue) > 0:
+        elem = q.dequeue(queue)
+        if elem not in visited['elements']:
+            al.add_last(visited,elem)
+            for nodo in G.adjacents(grafo,source)['elements']:
+                if nodo not in visited['elements']:
+                    q.enqueue(queue,nodo)
     return visited
